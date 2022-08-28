11bc99d9091cf875cc7c5dd2e736953af71d9c57
vineetkrdixit <vineetdixit.vns@gmail.com>
2022-08-28 12:39:40 +0530

    Staff login

--- lib-mgmt-ui/src/App.js
+++ lib-mgmt-ui/src/App.js
@@ -1,8 +1,9 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Login from './components/login/Login';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/login/Login";
+import Register from "./components/Register";
+import ARegister from "./components/admin/staffLogin/ARegister";
 
 function App() {
   return (
@@ -10,18 +11,18 @@ function App() {
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
+            <Link className="navbar-brand" to={"/sign-in"}>
               positronX
             </Link>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
+                  <Link className="nav-link" to={"/sign-in"}>
                     Login
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
+                  <Link className="nav-link" to={"/sign-up"}>
                     Sign up
                   </Link>
                 </li>
@@ -35,12 +36,12 @@ function App() {
             <Routes>
               <Route exact path="/" element={<Login />} />
               <Route path="/sign-in" element={<Login />} />
-              {/* <Route path="/sign-up" element={<SignUp />} /> */}
+              <Route path="/sign-up" element={<Register />} />
+              <Route path="/staff-sign-up" element={<ARegister />} />
             </Routes>
           </div>
         </div>
       </div>
-
     </Router>
   );
 }
---
+++ lib-mgmt-ui/src/components/admin/staffLogin/ARegister.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+const ARegister = () => {
+  return (
+    <>
+      <form>
+        <h3>Admin Sign Up</h3>
+        <div className="mb-3">
+          <label>Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Name"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Phone</label>
+          <input
+            type="tel"
+            className="form-control"
+            placeholder="Enter Phone Number"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter Email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter Password"
+          />
+        </div>
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </form>
+    </>
+  );
+};
+
+export default ARegister;
