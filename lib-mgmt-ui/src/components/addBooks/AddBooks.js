9f5f0fb38d36a0efce493696fecf0be683dd0b50
vineetkrdixit <vineetdixit.vns@gmail.com>
2022-08-30 16:07:40 +0530

    Created AddBook Page

--- lib-mgmt-ui/src/components/addBooks/AddBooks.js
+++ lib-mgmt-ui/src/components/addBooks/AddBooks.js
@@ -1,11 +1,69 @@
 import React from "react";
+import "../addBooks/AddBooks.css";
 
 const AddBooks = (props) => {
-    return (
-        <>
-            <h1>Add book component</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="container">
+        <h1 className="addBook mb-4">Add Book</h1>
+        <div className="row">
+          <div className="col-md-6 mb-4">
+            <label>Book Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Book Name"
+            />
+          </div>
+          <div className="col-md-6 mb-4">
+            <label>Author</label>
+            <input type="text" className="form-control" placeholder="Author" />
+          </div>
+          <div className="col-md-6 mb-4">
+            <label>Publisher</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Publisher"
+            />
+          </div>
+          <div className="col-md-6 mb-4">
+            <label>Genre</label>
+            <select className="form-control">
+              <option selected>Select Genre</option>
+              <option>Comedy</option>
+              <option>Fiction</option>
+              <option>Drama</option>
+              <option>Action</option>
+            </select>
+          </div>
+          <div className="col-md-6 mb-4">
+            <label>No Of Copies</label>
+            <input
+              type="Number"
+              className="form-control"
+              placeholder="No Of Copies"
+            />
+          </div>
+          <div className="col-md-6 mb-4 bookImage">
+            <label className="mb-4 ">Book Image</label>
+            <input type="file" class="form-control-file" />
+          </div>
+          <div className="col-md-6 mb-4">
+            <label>Serach Keys</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Serach Keys"
+            />
+          </div>
+        </div>
+        <div className="addBook">
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AddBooks
+export default AddBooks;
