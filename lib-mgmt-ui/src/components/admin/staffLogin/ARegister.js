94e08e816678b38ccf2ff05fbd230a0091afdcf8
Balraj <bhullar.bs@gmail.com>
2022-09-08 23:31:21 +0530

    FIX: UI for register component

--- lib-mgmt-ui/src/components/admin/staffLogin/ARegister.js
+++ lib-mgmt-ui/src/components/admin/staffLogin/ARegister.js
@@ -19,67 +19,68 @@ const ARegister = () => {
       email: emailInputRef.current.value,
       address: addressInputRef.current.value,
       password: passwordInputRef.current.value,
-      userType:"staff"
-    }
+      userType: "staff",
+    };
 
-    console.log({payload})
+    console.log({ payload });
     axios.post("http://localhost:3004/register", payload).then((res) => {
-      console.log(res)
+      console.log(res);
       navigate("/sign-in");
     });
-  }
+  };
 
   return (
     <>
-      <form>
-        <h3>Sign Up</h3>
-        <div className="mb-3">
-          <label>Name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter Name"
-            ref={nameInputRef}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Phone</label>
-          <input
-            type="tel"
-            className="form-control"
-            placeholder="Enter Phone Number"
-            ref={phoneInputRef}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Email</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter Email"
-            ref={emailInputRef}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Address</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Address"
-            ref={addressInputRef}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter Password"
-            ref={passwordInputRef}
-          />
-        </div>
+      <div className="auth-inner">
+        <form>
+          <h3>Sign Up</h3>
+          <div className="mb-3">
+            <label>Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Name"
+              ref={nameInputRef}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Phone</label>
+            <input
+              type="tel"
+              className="form-control"
+              placeholder="Enter Phone Number"
+              ref={phoneInputRef}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter Email"
+              ref={emailInputRef}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Address</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Address"
+              ref={addressInputRef}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter Password"
+              ref={passwordInputRef}
+            />
+          </div>
 
-        {/* <div className="mb-3">
+          {/* <div className="mb-3">
           <div className="custom-control custom-checkbox">
             <input
               type="checkbox"
@@ -91,15 +92,20 @@ const ARegister = () => {
             </label>
           </div>
         </div> */}
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary" onClick={registerHandler}>
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </form>
+          <div className="d-grid">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={registerHandler}
+            >
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            Forgot <a href="#">password?</a>
+          </p>
+        </form>
+      </div>
     </>
   );
 };
